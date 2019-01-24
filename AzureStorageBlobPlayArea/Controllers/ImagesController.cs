0f765d328a384381331b5ed89b7e2566fa7ab2e4
THG CSP2 <THG_CSP2@hensongroup.com>
2019-01-25 03:35:56 +0530

    Add project files.

---
+++ AzureStorageBlobPlayArea/Controllers/ImagesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AzureStorageBlobPlayArea.Models;
+using AzureStorageBlobPlayArea.Services;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorageBlobPlayArea.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private ImageStorageService _imageStoreService;
+
+        public ImagesController()
+        {
+            _imageStoreService = new ImageStorageService();
+        }
+
+        public ActionResult Index()
+        {
+            List<CloudBlockBlob> blobs = _imageStoreService.GetImages();
+
+            return View(blobs);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Upload(HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                string imageId = await _imageStoreService.SaveImage(image.InputStream);
+                return RedirectToAction("ShowImage", new { id = imageId });
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult ShowImage(string id)
+        {
+            // use this for container that has access type set to Blob
+            // Uri imageUri = _imageStoreService.GetResourceUri(id);
+
+            var viewModel = new ImageViewerViewModel()
+            {
+               Uri = _imageStoreService.GetResourceUriWithSas(id),
+               ResourceId = id
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteImage(string id)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id));
+
+            await _imageStoreService.DeleteImage(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AzureStorageBlobPlayArea/Services/ImageStorageService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorageBlobPlayArea.Services
+{
+    public class ImageStorageService
+    {
+        private readonly CloudBlobClient _cloudBlobClient;
+        private readonly CloudBlobContainer _container;
+        private readonly CloudBlockBlob _blob;
+        private readonly Uri _blobServiceEndpoint = new Uri(ConfigurationManager.AppSettings.Get("blobServiceEndpoint"));
+
+        public ImageStorageService()
+        {
+            var credentials = new StorageCredentials("botteststorage1", "2Bt8LgFrsUBPyrd/9BaU+X0fNZ/mxQ5/tOEWqlaqI1CEu+hfJOaOMOqo7afK6vWr3U+PF6CK7H7aTUpTgxwOMg==");
+            _cloudBlobClient = new CloudBlobClient(_blobServiceEndpoint, credentials);
+            _container = _cloudBlobClient.GetContainerReference("private-images");
+        }
+
+        public Uri GetResourceUri(string resourceId)
+        {
+            return new Uri(_blobServiceEndpoint, $"/images/{resourceId}");
+        }
+
+       
+
+        public Uri GetResourceUriWithSas(string resourceId)
+        {
+            var sasPolicy = new SharedAccessBlobPolicy()
+            {
+                Permissions = SharedAccessBlobPermissions.Read,
+                SharedAccessStartTime = DateTime.Now.AddMinutes(-10),
+                SharedAccessExpiryTime = DateTime.Now.AddMinutes(30)
+            };
+
+            CloudBlockBlob blob = _container.GetBlockBlobReference(resourceId);
+            string sasToken = blob.GetSharedAccessSignature(sasPolicy);
+
+            return new Uri(_blobServiceEndpoint, $"/private-images/{resourceId}{sasToken}.ToString()");
+
+        }
+
+        public async Task<string> SaveImage(Stream imageInputStream)
+        {
+            string id = Guid.NewGuid().ToString();
+            CloudBlockBlob blob = _container.GetBlockBlobReference(id);
+
+            await blob.UploadFromStreamAsync(imageInputStream);
+
+            return id;
+        }
+
+        public List<CloudBlockBlob> GetImages()
+        {
+            var blockBlobs = new List<CloudBlockBlob>();
+            IEnumerable<IListBlobItem> blobs = _container.ListBlobs();
+            IList<IListBlobItem> blobItems = blobs as IList<IListBlobItem> ?? blobs.ToList();
+
+            if (blobItems.Count == 0) return blockBlobs;
+
+            foreach (IListBlobItem blobItem in blobItems)
+            {
+                if (blobItem is CloudBlockBlob)
+                {
+                    blockBlobs.Add(blobItem as CloudBlockBlob);
+                }    
+            }
+
+            return blockBlobs;
+        }
+
+        public async Task<bool> DeleteImage(string resourceId)
+        {
+            CloudBlockBlob blob = _container.GetBlockBlobReference(resourceId);
+
+            return await blob.DeleteIfExistsAsync();
+        }
+
+       
+
+    }
+}
